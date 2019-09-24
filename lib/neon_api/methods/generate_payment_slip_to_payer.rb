c1cb990462081219ef82bab016a28f61496cd5bc
Lucas Perez <lucascperez@gmail.com>
2019-09-24 16:28:35 -0300

    putting all params in boleto method

--- lib/neon_api/methods/generate_payment_slip_to_payer.rb
+++ lib/neon_api/methods/generate_payment_slip_to_payer.rb
@@ -1,11 +1,11 @@
 module NeonApi
   module GeneratePaymentSlipToPayer
     URL = 'V1/PaymentSlip/GeneratePaymentSlipToPayer'
-    def self.create(bank_account, payer_id, cpf, due_date, value, instructions = [])
-      NeonApi.client.send_request(payload(bank_account, payer_id, cpf, due_date, value, instructions = []), URL)
+    def self.create(bank_account, payer_id, cpf, due_date, value, instructions = [], send_mail_to_payer = false, description="Boleto de Teste")
+      NeonApi.client.send_request(payload(bank_account, payer_id, cpf, due_date, value, instructions, send_mail_to_payer, description), URL)
     end
 
-    def self.payload(bank_account, payer_id, cpf, due_date, value, instructions = [])
+    def self.payload(bank_account, payer_id, cpf, due_date, value, instructions = [], send_mail_to_payer = false, description="Boleto de Teste")
       {
           "ClientID": NeonApi.client.client_id,
           "BankAccountId": bank_account,
@@ -13,7 +13,9 @@ module NeonApi
           "CPF": cpf,
           "DueDate": due_date,
           "Value": value,
-          "Instructions": instructions
+          "Instructions": instructions,
+          "SendEmailToPayer": send_mail_to_payer,
+          "Description": description
       }.to_json
     end
   end
